d64a15429c3a094d9eb55893ebcad2bbc454c4e0
yassergithub1 <yassergithub1@gmail.com>
2021-03-11 22:28:23 +0000

    added task a and b

--- python.py
+++ python.py
@@ -1,23 +1,22 @@
-# e) Write a program containing a function to check if a user inputted string is a good
-#    password or not, if not have them try again. A password is considered good if it
-#    contains at least 7 characters and 2 of those are either a number or special
-#    character(by special character I mean any one of the characters on the numbers
-#    1-8, i.e. !@#$%^&*).
+#Name: Yasser
+#Email: yassergithub1@gmail.com
 
+# a) Write a program containing a function to reverse a user inputted string.
 
-import re
-p= input("Input your password")
-x = True
-count = 0
-if(len(p)>7):
-    for i in range(len(p)):  
-        if re.search("[0-9]",p):
-            count += 1
-        if re.search("[!@#$%^&*]",p):
-            count += 1
+string=input()
+stringlength = len(string)
+reversedString = string[stringlength::-1] 
+print(slicedString)
 
-    if(count >= 2):
-        print("Valid Password")
-        x = False
-if x:
-    print("Not a Valid Password")
+# b) Write a program containing a function to check if a user inputted number is prime
+
+num = int(input())
+if num > 1:
+    for i in range(2,num):
+        if (num % i) == 0:
+            print(num,"is not a prime number")
+            break
+    else:
+        print(num,"is a prime number")
+else:
+    print(num,"is not a prime number")
